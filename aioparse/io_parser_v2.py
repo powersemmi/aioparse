e0b0a69d76b077c4a59b097296c768e15ad4009b
powersemmi <powersemmi@gmail.com>
2019-12-20 17:54:44 +0300

    mypy doing

--- aioparse/io_parser_v2.py
+++ aioparse/io_parser_v2.py
@@ -1,10 +1,11 @@
 import asyncio
 import os
+from asyncio import Future
 from concurrent.futures.process import ProcessPoolExecutor
-from typing import Tuple, List, Callable, Dict
+from typing import List, Callable, Dict, Union, Any
 
-from aiologger import Logger
 import aiohttp
+from aiologger import Logger
 
 
 class AIOParse:
@@ -13,11 +14,13 @@ class AIOParse:
         self.root_urls = root_urls
         self.parse_funcs = parse_funcs
         self.logger = Logger.with_default_handlers()
+        self.page = []
 
-    async def parse(self, url: str) -> List[str]:
-        pass
+    async def parse(self, page: List) -> Union[asyncio.Future[Any], List[str]]:
+        self.page[1] = page
+        return self.page
 
-    async def request(self, client, url):
+    async def request(self, client, url) -> asyncio.Future:
         # time.sleep(0.1)
         self.logger.debug(f"[GET] {url} start")
         if "h" in url:
@@ -35,33 +38,36 @@ class AIOParse:
                 except aiohttp.ServerTimeoutError:
                     self.logger.debug(f"[GET] ServerTimeoutError {url} ERROR")
                     continue
-                except:
-                    continue
         return result
 
-    async def crawl(self, future: asyncio.Future or List[str], client, pool):
-        futures = []
+    async def crawl(self, future: Union[List[Union[str, Future[Any]]], Future[Any], Any],
+                    client, pool) -> None:
+        futures: Any = []
         # Получаем из футуры ссылки
-        try:
-            urls: List[str] = await future
+        if isinstance(future, Future):
+            urls: Union[Any] = await future
             # Выгребаем для каждой ссылки разметку страницы
-            for request_future in asyncio.as_completed([self.request(client, url) for url in urls]):
-                parse_future: asyncio.Future = [await self.parse(await request_future)]
+            for request_future in asyncio.as_completed([self.request(client, url)
+                                                        for url in urls]):
+                parse_future: List[Union[Future[Any],
+                                         List[str]]] = [await self.parse(await request_future)]
 
                 futures.append(asyncio.ensure_future(self.crawl(parse_future, client, pool)))
             if futures:
                 await asyncio.wait(futures)
-        except TypeError:
+        elif isinstance(future, List):
             # Выгребаем для каждой ссылки разметку страницы
             for urls in future:
-                for request_future in asyncio.as_completed([self.request(client, url) for url in urls]):
-                    parse_future: List[str] = [await self.parse(await request_future)]
+                for request_future in asyncio.as_completed([self.request(client, url)
+                                                            for url in urls]):
+                    parse_future: List[Union[str, Future[Any]]] = \
+                        [await self.parse(await request_future)]
 
                     futures.append(asyncio.ensure_future(self.crawl(parse_future, client, pool)))
                 if futures:
                     await asyncio.wait(futures)
-        except:
-            pass
+        # else:
+        #     pass
 
     async def main(self):
         # Создаём пул потоков по количеству процессоров
@@ -73,7 +79,6 @@ class AIOParse:
                 initial_future = self.event_loop.create_future()
                 # Помещаем в неё ссылки, с которых начнём парсить
                 initial_future.set_result(self.root_urls)
-                # Передаём эту футуру в сопрограмму обхода ссылок
-                # вместе с пулом потоков и клиентской сессией
+                # Передаём эту футуру в сопрограмму обхода
                 await self.crawl(initial_future, client, pool)
 
